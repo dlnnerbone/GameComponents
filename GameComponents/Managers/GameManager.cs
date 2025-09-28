c557510351ea003501887c0dc7c5939ed394d22d
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-09-28 14:51:10 -0400

    updating library before restoring tools

--- GameComponents/Managers/GameManager.cs
+++ GameComponents/Managers/GameManager.cs
@@ -2,11 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 namespace GameComponents.Managers;
-public abstract class GameManager 
+public abstract class Scene 
 {
-    public virtual void Initialize(Game game) {}
-    public virtual void LoadContent(GraphicsDevice Device, ContentManager Manager) {}
-    public virtual void UnloadContent(ContentManager manager) {}
-    public virtual void UpdateLogic(GameTime GT) {}
-    public virtual void Draw(SpriteBatch batch) {}
+    public virtual ContentManager SceneContent { get; }
 }
--- obj/Debug/net8.0/GameComponents.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameComponents.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameComponents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8c531438a75b5a7c2e0126b33860fc617d4008e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c628907437dd94aa2f59eebc7882f415a8daab7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameComponents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameComponents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

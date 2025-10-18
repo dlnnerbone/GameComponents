11fa35801af0812aef8a84bf15e19a5d76b6d345
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-10-17 22:56:14 -0400

    adding new delegate method.

--- obj/Debug/net8.0/GameComponents.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameComponents.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameComponents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55553978e303d78464407437b5005198fbe62df4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d4e892273e78536bfaf37f81ff709e64b5b6b1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameComponents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameComponents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

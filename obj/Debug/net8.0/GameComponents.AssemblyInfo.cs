ac027ce58298b967e8f4b873bafb673938601e8b
dlnnerbone <ameenalmoosawi1234@gmail.com>
2025-10-18 15:17:01 -0400

    adding obj and bin directories to .gitignore file.

--- obj/Debug/net8.0/GameComponents.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameComponents.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameComponents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1aee3d56545382d645f069fcef0163168d7e67e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af299bd102f11d0cc4dbe49dd51d851919d72222")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameComponents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameComponents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

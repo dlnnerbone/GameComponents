62ac585ed40b2af12e5a85116968f5ca783a9d0e
dlnnerbone <ameenalmoosawi1234@gmail.com>
2026-02-22 12:28:19 -0500

    updating this branch because I switched to a new OS.

--- obj/Debug/net8.0/GameComponents.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameComponents.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameComponents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e3dde2aede354b86e52ddc0766983d28fcabb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0410c661fc534660518b8534126698e0c55ef63")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameComponents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameComponents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
